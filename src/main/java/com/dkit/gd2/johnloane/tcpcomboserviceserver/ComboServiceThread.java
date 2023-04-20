a8d2c82bbb38e8c97a3852e9c3187203d54546c6
John Loane <john.loane@dkit.ie>
2023-04-20 15:00:53 +0100

    Multi-thread TCP Combo Service

--- src/main/java/com/dkit/gd2/johnloane/tcpcomboserviceserver/ComboServiceThread.java
+++ src/main/java/com/dkit/gd2/johnloane/tcpcomboserviceserver/ComboServiceThread.java
@@ -1,9 +1,12 @@
 package com.dkit.gd2.johnloane.tcpcomboserviceserver;
 
+import com.dkit.gd2.johnloane.tcpcomboservicecore.TCPComboServiceDetails;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Date;
 import java.util.Scanner;
 
 public class ComboServiceThread extends Thread
@@ -15,6 +18,7 @@ public class ComboServiceThread extends Thread
 
     //Constructor needs:
     //1. A socket to communicate with the client
+
     public ComboServiceThread(ThreadGroup group, String name, Socket dataSocket, int number)
     {
         super(group, name);
@@ -34,5 +38,86 @@ public class ComboServiceThread extends Thread
         }
     }
 
-    
+    //run method. This is called when .start() is called on an instance of teh Thread class. The run method should contain the logic for
+    //a) receiving a message from the client
+    //b) processing the message
+    //c) sending a response to the client
+    @Override
+    public void run()
+    {
+        String incomingMessage = "";
+        String response = "";
+        try
+        {
+            //While the client doesn't want to quit
+            while (!incomingMessage.equals(TCPComboServiceDetails.QUIT_COMMAND))
+            {
+                //Wipe the respinse to make sure we don't send old values
+                response = null;
+                //Read a message from the client
+                incomingMessage = input.nextLine();
+                System.out.println("Server received: " + incomingMessage);
+
+                //Break up the message into its components
+                String[] messageParts = incomingMessage.split(TCPComboServiceDetails.BREAKING_CHARACTERS);
+
+                //Process the message
+                if (messageParts[0].equals(TCPComboServiceDetails.ECHO_COMMAND))
+                {
+                    System.out.println("Echo" + incomingMessage);
+                    StringBuffer echoMessage = new StringBuffer("");
+                    if(messageParts.length > 1)
+                    {
+                        echoMessage.append(messageParts[1]);
+                        for(int i = 2; i < messageParts.length; i++)
+                        {
+                            echoMessage.append(TCPComboServiceDetails.BREAKING_CHARACTERS);
+                            echoMessage.append(messageParts[i]);
+                        }
+                    }
+                    response = echoMessage.toString();
+                }
+                else if (messageParts[0].equals(TCPComboServiceDetails.DAYTIME_COMMAND))
+                {
+                    response = new Date().toString();
+                }
+                else if (incomingMessage.equals(TCPComboServiceDetails.STATS_COMMAND))
+                {
+                    response = "7";
+                }
+                else if (incomingMessage.equals(TCPComboServiceDetails.QUIT_COMMAND))
+                {
+                    response = TCPComboServiceDetails.SESSION_TERMINATED;
+                }
+                else
+                {
+                    response = TCPComboServiceDetails.UNRECOGNISED;
+                }
+
+                //Send a response to the client
+                output.println(response);
+                System.out.println("Server sent: " + response);
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(incomingMessage);
+            System.out.println("An exception occurred with client number #" + number +": " + e.getMessage());
+        }
+        finally
+        {
+            try
+            {
+                //Shut down the connection
+                System.out.println("Closing the connecting with client number #" + number);
+                dataSocket.close();
+            }
+            catch(IOException e)
+            {
+                System.out.println("Unable to disconnect with client number #" + number);
+                System.exit(1);
+            }
+        }
+
+    }
 }
--- src/main/java/com/dkit/gd2/johnloane/tcpcomboserviceserver/ComboStreamServer.java
+++ src/main/java/com/dkit/gd2/johnloane/tcpcomboserviceserver/ComboStreamServer.java
@@ -32,7 +32,7 @@ public class ComboStreamServer
                 Socket dataSocket = listeningSocket.accept();
 
                 numberClients++;
-                System.out.println("Server has not accepted " + numberClients + " clients");
+                System.out.println("Server has now accepted " + numberClients + " clients");
 
                 //Build the thread to handle the client
                 //Thread needs:
@@ -40,64 +40,9 @@ public class ComboStreamServer
                 //2. A name to listed under
                 //3. A socket to communicate through
                 //4. Maybe other stuff
-                ComboServiceThread newClient = new ComboServiceThread(clientThreads, dataSocket.getInetAddress()+"", dataSocket, numberClients);
+                ComboServiceThread newClient = new ComboServiceThread(clientThreads, dataSocket.getInetAddress() + "", dataSocket, numberClients);
                 newClient.start();
 
-
-                //Build input and output streams
-                OutputStream out = dataSocket.getOutputStream();
-                PrintWriter output = new PrintWriter(new OutputStreamWriter(out));
-
-                InputStream in = dataSocket.getInputStream();
-                Scanner input = new Scanner(new InputStreamReader(in));
-
-                String incomingMessage = "";
-                String response;
-
-                while(!incomingMessage.equals(TCPComboServiceDetails.QUIT_COMMAND))
-                {
-                    response = null;
-                    //See what the client wants to do
-                    incomingMessage = input.nextLine();
-                    System.out.println("Message received: " + incomingMessage);
-
-                    //Break the message into its components
-                    String[] components = incomingMessage.split(TCPComboServiceDetails.BREAKING_CHARACTERS);
-
-                    if(components[0].equals(TCPComboServiceDetails.ECHO_COMMAND))
-                    {
-                        //Take the remaining text that the client sent and echo it back
-                        StringBuffer echoMessage = new StringBuffer("");
-                        if(components.length > 1)
-                        {
-                            echoMessage.append(components[1]);
-                            //What is the user used %% with their message?
-                            for(int i = 2; i < components.length; i++)
-                            {
-                                echoMessage.append(TCPComboServiceDetails.BREAKING_CHARACTERS);
-                                echoMessage.append(components[i]);
-                            }
-                        }
-                        response = echoMessage.toString();
-                    }
-                    else if(components[0].equals(TCPComboServiceDetails.DAYTIME_COMMAND))
-                    {
-                        response = new Date().toString();
-                    }
-                    else if(components[0].equals(TCPComboServiceDetails.STATS_COMMAND))
-                    {
-                        response = "7";
-                    }
-                    else
-                    {
-                        response = TCPComboServiceDetails.UNRECOGNISED;
-                    }
-
-                    //Send the response back to the client
-                    output.println(response);
-                    output.flush();
-                }
-                dataSocket.close();
 
             }
             listeningSocket.close();
